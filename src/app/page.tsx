a13b4fa22d6c9c5e013da24dbcfc910ae8c7fc9e
Anagha <anaghajayaprakash1996@gmail.com>
2023-09-26 22:52:17 -0500

    feat: Set up home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-import { Port_Lligat_Sans, Source_Code_Pro } from "next/font/google"
+import { Fira_Code } from "next/font/google"
 import Image from "next/image";
+import style from "./page.module.css"
+import Header from "./header";
 
-const sourceCodePro = Source_Code_Pro({subsets: ['latin']});
+const firaCode = Fira_Code({subsets: ['latin']});
 
 export default function Home() {
   return (
-    <div className={sourceCodePro.className}>
-      <h1>Hello!</h1>
+    <div className={firaCode.className + ' ' + style.home} style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', margin: '24px'}}>
+      <Header></Header>
+      <div>
+        <h1>Anagha Jayaprakash, </h1>
+        <h1>Software Engineer</h1>
+        <h4 style={{opacity: '80%'}}>Mobile Application Development | Web Development </h4>
+      </div>
+      <div style={{width: '25%', height: '25%', position: 'relative'}}>
+        <Image className={style.profileImage} src={'/Memoji_Hi.png'} width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} alt=""></Image>
+      </div>
     </div>
   )
 }
