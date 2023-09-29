e5b48f7e419e3c540311793c39721aa153213537
Anagha <anaghajayaprakash1996@gmail.com>
2023-09-29 16:34:52 -0500

    feat: Complete home screen with about section

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,7 +3,6 @@ import header from './Styles/header.module.css'
 export default function Header() {
     return (
         <div className={header.header} style={{}}>
-            <span style={{width: '100%'}}></span>
             <a href="">Home</a>
             <a href="">Expertise</a>
             <a href="">Projects</a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,9 @@ const firaCode = Fira_Code({subsets: ['latin']});
 
 export default function Home() {
   return (
-    <div className={firaCode.className} style={{verticalAlign: 'middle', height: '100%'}}>
-        <Header></Header>
-      <div className={style.home} style={{display: 'flex', margin: '24px', height: '100%'}}>
+    <div className={firaCode.className} style={{height: '100%'}}>
+      <Header></Header>
+      <div className={style.home} style={{display: 'flex', margin: '24px', alignItems: "center", height: "100%"}}>
         <div>
           <h1>Anagha Jayaprakash, </h1>
           <h1>Software Engineer</h1>
@@ -20,6 +20,7 @@ export default function Home() {
         <div className={style.profileImageDiv} >
           <Image className={style.profileImage} src={'/Memoji_Hi.png'} width={0} height={0} sizes="100vw" alt="" style={{ width: '100%', height: 'auto' }}></Image>
         </div>
+        <h3> I'm passionate about crafting mobile and web apps from the ground up. With a keen eye for design and a commitment to user-centric solutions, I specialize in creating applications with intuitive interfaces that elevate the user experience.</h3>
       </div>
     </div>
   )
